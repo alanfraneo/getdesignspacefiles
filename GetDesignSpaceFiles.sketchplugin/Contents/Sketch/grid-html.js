fdf12f87698c596e7a17b5c6194e02bd4b633d7c
Alan Francis <alanfraneo@gmail.com>
2019-07-23 17:32:04 +0530

    static file versions updated

--- GetDesignSpaceFiles.sketchplugin/Contents/Sketch/grid-html.js
+++ GetDesignSpaceFiles.sketchplugin/Contents/Sketch/grid-html.js
@@ -5,12 +5,12 @@ GridHTML.getHTML = function (context, config) {
   var neogalleryjs = GridHTML.getJS(context);
   var html =
     '<!DOCTYPE HTML><html><head><meta charset=utf-8><title>NeoGallery</title>\
-      <link href="/bi/webapp/css/neogallery.v2.css" rel="stylesheet" type="text/css" />\
+      <link href="/bi/webapp/css/neogallery.v3.css" rel="stylesheet" type="text/css" />\
       </head><body><div id="title"></div>\
       <div id="tabs"></div>\
       <div id="neogallery"></div></body>\
       <script>var imgconfig = '+config+';</script>\
-      <script src="/bi/webapp/js/neogallery.v2.js"></script>\
+      <script src="/bi/webapp/js/neogallery.v3.js"></script>\
      </html>';
   return html;
 };
